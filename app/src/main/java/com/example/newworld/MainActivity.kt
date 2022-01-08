f158269cced3ee380cc163bb4a13a262052914d8
MarioHdez <mariohdez.developer@gmail.com>
2022-01-07 23:26:27 -0800

    Variables & Constants

--- app/src/main/java/com/example/newworld/MainActivity.kt
+++ app/src/main/java/com/example/newworld/MainActivity.kt
@@ -7,5 +7,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        variablesYConstantes()
+    }
+
+    private fun variablesYConstantes() {
+        // Variables
+
+        var myVar= "Hello World"
+        println(myVar)
+
+        myVar = 1.toString() // Convert int to string
+        println(myVar)
+
+        // Constants
+
+        val constant = "My Constant" // Cannot be changed
+        println(constant)
     }
 }
