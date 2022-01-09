c9efdd6319cf050681b1e528e079ce3cae1a31e5
MarioHdez <mariohdez.developer@gmail.com>
2022-01-08 18:52:51 -0800

    Arrays

--- app/src/main/java/com/example/newworld/MainActivity.kt
+++ app/src/main/java/com/example/newworld/MainActivity.kt
@@ -12,15 +12,53 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun myFunc() {
-        // WHEN (SWITCH)
-        val country = "Mexico"
+        // ARRAY
 
-        when(country) {
-            "Spain" ->  println("This is Spain")
-            "Canada" -> println("This is Canada")
-            "Mexico" -> println("This is Mexico")
-            else -> println("Where i am?")
+        val name = "Raquel"
+        val surname = "Sanabria"
+        val company = "Twitter"
+        val age = "21"
+
+        // Creation
+        val raquel: ArrayList<String> = arrayListOf<String>()
+
+        // Add data
+        raquel.add(name)
+        raquel.add(name) // Accept duplicated data
+        raquel.add(surname)
+        raquel.add(company)
+        raquel.add(age)
+
+        // Print
+        println(raquel)
+
+        // Add a list of data
+        raquel.addAll(listOf("Blacked", "Waifu"))
+        raquel.addAll(raquel)
+        println(raquel)
+
+        // Data Access
+        println(raquel[0])
+
+        // Modify Data
+        raquel[0] = "Raquel <3"
+        println(raquel[0])
+
+        // Remove Data
+        raquel.removeAt(0)
+        println(raquel)
+
+        // Iteration
+        raquel.forEach{
+            println(it)
         }
 
+        // OPERATIONS
+        // Array Size
+        println("Raquel length: ${raquel.count()}")
+
+        // Clear Array
+        raquel.clear()
+        println("Raquel length: ${raquel.count()}")
     }
 }
